1bd83427ae14b291648d95c25884ed31cdac562f
dj8639rsm <dj8639rsm@gmail.com>
2020-04-25 18:03:10 +0900

    BindigContext実装

--- xamarinApp/xamarinApp/MainPage.xaml.cs
+++ xamarinApp/xamarinApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace xamarinApp
 {
@@ -12,11 +14,47 @@ namespace xamarinApp
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
 
-    public static class StaticItem
+
+    public class BindableBase:INotifyPropertyChanged
     {
-        public static string Message { get; } = "hello static world";
+        protected BindableBase()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnProretyChaned([CallerMemberName]string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if(object.Equals(field, value)) { return false; }
+
+            field = value;
+
+            this.OnProretyChaned(propertyName);
+            return true;
+        }
     }
 
+    public class MyPageViewModel:BindableBase
+    {
+        private double sliderValue;
+
+        public double SliderValue
+        {
+            get { return this.sliderValue; }
+
+            set { this.SetProperty(ref this.sliderValue, value); this.OnProretyChaned(nameof(LabelValue)); }
+        }
+
+        public string LabelValue => string.Format("This is slider value '{0}'", this.SliderValue);
+    }
+
+    
 
     public partial class MainPage : ContentPage
     {
