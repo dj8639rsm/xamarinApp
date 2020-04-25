a257e3a93f756ec5dffdf3a943236ecebf1c818c
dj8639rsm <dj8639rsm@gmail.com>
2020-04-25 16:18:28 +0900

    プロジェクト ファイルを追加します。

---
+++ xamarinApp/xamarinApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarinApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+
+    public static class StaticItem
+    {
+        public static string Message { get; } = "hello static world";
+    }
+
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        
+    }
+}
